64a3c6fb71461e5bccf78942e10c123ae4fa4464
Bradley Colquitt <bradley.colquitt@gmail.com>
2018-03-20 20:51:49 -0700

    added adaptive thresholding

--- lib/circular_buffer.h
+++ lib/circular_buffer.h
@@ -8,8 +8,8 @@ typedef struct {
 } circular_buf_t;
 
 int circular_buf_reset(circular_buf_t * cbuf);
-int circular_buf_put(circular_buf_t * cbuf, uint8_t data);
-int circular_buf_get(circular_buf_t * cbuf, uint8_t * data);
+int circular_buf_put(circular_buf_t * cbuf, float * data);
+int circular_buf_get(circular_buf_t * cbuf, float * data);
 bool circular_buf_empty(circular_buf_t cbuf);
 bool circular_buf_full(circular_buf_t cbuf);
 void circular_buf_init(circular_buf_t * cbuf, int size);
