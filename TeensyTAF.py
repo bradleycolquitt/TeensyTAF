ccaa5067a9c6c8568fef831b1003b072dbe5bdcd
lyre <bradley.colquitt@gmail.com>
2018-03-21 17:25:47 -0700

    small tweaks

--- TeensyTAF.py
+++ TeensyTAF.py
@@ -11,6 +11,7 @@ import time
 import datetime
 import serial
 import multiprocessing as mp
+import lib.GS_timing as gs
 from serial.tools import list_ports
 
 
@@ -23,10 +24,14 @@ from serial.tools import list_ports
 # outfile=open(outfile_fn,'a')
 
 
+        
 class TeensyTAF:
     def __init__(self):
         self.inital_date = datetime.datetime.now()
-        self.serial_con = serial.Serial("/dev/ttyACM0",115200)
+        dev_path = "/dev"
+        possible_cons = ['/'.join([dev_path, a]) for a in os.listdir(dev_path) if re.search("ACM", a)]
+        print(possible_cons)
+        self.serial_con = serial.Serial(possible_cons[0],115200)
         self.audio_recorder = at.AudioRecord()
 
     def init_config(self, config_file):
@@ -52,12 +57,16 @@ def serial_recorder_loop(serial_con, serial_fp):
     while True:
         if (serial_con.inWaiting()>0):
             line=serial_con.readline(serial_con.inWaiting()) #.strip('\r'));
-            line_out = ''.join([line_out, line.strip('\r\n')])
+            line_out = ','.join([line_out, line.strip('\r\n')])
 
             #if re.search('PSD', line):
                 
             if re.search('\n', line):
-                line_out = ','.join([str(time.time()), str(event_num), line_out])
+                #line_out = ','.join([str(time.time())
+                #                     , str(event_num), line_out])
+                line_out = ','.join([str(gs.millis()/1000),
+                                         str(event_num), line_out])
+                line_out = re.sub(',,',',', line_out)
                 serial_fp.write(line_out + '\n');
                 serial_fp.flush()
                 print(line_out)
@@ -72,6 +81,7 @@ def serial_recorder_loop_json(serial_con, serial_fp):
             line_out = ''.join([line_out, line.strip('\r\n')])
             if re.search('\n', line):
                 line_out = ','.join([str(time.time()), line_out])
+                
                 serial_fp.write(line_out + '\n');
                 serial_fp.flush()
                 print(line_out)
